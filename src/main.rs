1102a8275e6623fb7938d49d738c4a7fa11b5f26
spooky-th-ghost <colin@wildheartinteractive.com>
2024-05-23 10:16:00 -0400

    infinite bug

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy_rapier3d::prelude::*;
+use bevy_xpbd_3d::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins,
-            RapierPhysicsPlugin::<NoUserData>::default(),
-            RapierDebugRenderPlugin::default(),
+            bevy_xpbd_3d::prelude::PhysicsPlugins::default(),
+            // bevy_xpbd_3d::prelude::PhysicsDebugPlugin::default(),
             WorldInspectorPlugin::default(),
         ))
-        .insert_resource(RapierConfiguration {
-            gravity: Vec3::NEG_Y * 30.0,
-            ..default()
-        })
         .register_type::<GroundSensor>()
         .register_type::<WireInfo>()
         .insert_resource(WireInfo::default())
@@ -22,12 +18,7 @@ fn main() {
         .add_systems(
             Update,
             (
-                (
-                    deploy_stake,
-                    move_player,
-                    handle_ground_sensor,
-                    (handle_wire_length, create_segments).chain(),
-                ),
+                (deploy_stake, move_player, handle_wire_length),
                 apply_deferred,
             )
                 .chain(),
@@ -89,30 +80,11 @@ impl Default for WireInfo {
     }
 }
 
-#[derive(Bundle)]
-pub struct SegmentBundle {
-    transform: TransformBundle,
-    collider: Collider,
-    rigidbody: RigidBody,
-    impulse_joint: ImpulseJoint,
-    segment: Segment,
-    name: Name,
-}
-
-impl SegmentBundle {
-    pub fn new(translation: Vec3, impulse_joint: ImpulseJoint) -> Self {
-        SegmentBundle {
-            transform: TransformBundle {
-                local: Transform::from_translation(translation),
-                ..default()
-            },
-            collider: Collider::capsule_x(0.4, 0.15),
-            rigidbody: RigidBody::Dynamic,
-            impulse_joint,
-            segment: Segment,
-            name: Name::from("Segment"),
-        }
-    }
+#[derive(PhysicsLayer, Clone, Copy, Debug)]
+pub enum Layers {
+    Ground,
+    Player,
+    Chain,
 }
 
 #[derive(Resource)]
@@ -149,155 +121,148 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
+    let chain_layers = CollisionLayers::new(Layers::Chain, Layers::Ground);
+
     commands.insert_resource(WireAssets {
         stake: asset_server.load("stake.glb#Scene0"),
         segment: asset_server.load("segment.glb#Scene0"),
     });
 
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 8.0, -15.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 10.0, -15.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
     commands.spawn((
         PbrBundle {
             transform: Transform::from_translation(Vec3::NEG_Y * 4.0),
-            mesh: meshes.add(shape::Box::new(200.0, 4.0, 200.0).into()),
-            material: materials.add(Color::GOLD.into()),
+            mesh: meshes.add(Cuboid::new(100.0, 2.0, 100.0)),
+            material: materials.add(Color::GOLD),
             ..default()
         },
         Collider::cuboid(100.0, 2.0, 100.0),
-        RigidBody::Fixed,
+        CollisionLayers::new(Layers::Ground, Layers::Player),
+        RigidBody::Static,
     ));
 
     let mut next_entity = commands
         .spawn((
             PbrBundle {
-                mesh: meshes.add(
-                    shape::Capsule {
-                        radius: 0.5,
-                        depth: 1.0,
-                        rings: 4,
-                        ..default()
-                    }
-                    .into(),
-                ),
-                material: materials.add(Color::BLUE.into()),
+                mesh: meshes.add(Capsule3d {
+                    radius: 0.5,
+                    half_length: 0.5,
+                }),
+                material: materials.add(Color::RED),
                 ..default()
             },
             Player,
+            Name::from("Player"),
+            CollisionLayers::new(Layers::Player, Layers::Ground),
             LockedAxes::ROTATION_LOCKED,
             RigidBody::Dynamic,
-            Velocity::default(),
-            Collider::capsule_y(0.5, 0.5),
+            Collider::capsule(0.5, 0.5),
             GroundSensor::default(),
         ))
         .id();
 
-    let mut joint = SphericalJointBuilder::new()
-        .local_anchor1(Vec3::X * 0.35)
-        .local_anchor2(Vec3::NEG_X * 0.35)
-        .build();
-
-    joint.set_contacts_enabled(false);
-
-    let starting_translation = Vec3::new(-4.0, -1.0, 4.0);
-
-    for i in 0..20 {
-        let translation = (0.3 * i as f32 * Vec3::X) + starting_translation;
-        next_entity = commands
-            .spawn((
-                SegmentBundle::new(translation, ImpulseJoint::new(next_entity, joint)),
-                Damping {
-                    linear_damping: 2.0,
-                    angular_damping: 3.0,
-                },
-            ))
-            .id();
+    fn segment_translation(i: usize) -> Vec3 {
+        let starting_translation = Vec3::new(-4.0, -1.0, 4.0);
+        (0.3 * i as f32 * Vec3::X) + starting_translation
     }
 
-    let mut stake_joint = SphericalJointBuilder::new()
-        .local_anchor1(Vec3::X)
-        .local_anchor2(Vec3::ZERO)
-        .build();
+    let chain_iterations = 20;
 
-    stake_joint.set_contacts_enabled(false);
+    for i in 0..chain_iterations {
+        let previous_entity = next_entity;
+        let translation = segment_translation(i);
+        next_entity = commands
+            .spawn((
+                PbrBundle {
+                    mesh: meshes.add(Capsule3d {
+                        radius: 0.125,
+                        half_length: 0.125,
+                    }),
+                    material: materials.add(Color::BLUE),
+                    transform: Transform::from_translation(translation),
+                    ..default()
+                },
+                RigidBody::Dynamic,
+                Collider::capsule(0.5, 0.25),
+                chain_layers,
+                Name::from("Link"),
+            ))
+            .id();
 
-    commands.spawn((
-        SceneBundle {
-            transform: Transform::from_xyz(5.0, -1.5, 4.0),
-            scene: asset_server.load("stake.glb#Scene0"),
-            ..default()
-        },
-        RigidBody::Fixed,
-        Collider::cuboid(0.25, 0.5, 0.25),
-        Stake,
-        Name::from("Stake"),
-        ImpulseJoint::new(next_entity, stake_joint),
-    ));
+        commands.spawn(
+            DistanceJoint::new(previous_entity, next_entity)
+                .with_limits(0.01, 0.05)
+                .with_local_anchor_1(Vec3::Y)
+                .with_local_anchor_2(Vec3::NEG_Y),
+        );
+    }
+
+    let chain_last_entity = next_entity;
+
+    next_entity = commands
+        .spawn((
+            PbrBundle {
+                mesh: meshes.add(Cuboid::new(0.5, 2.0, 0.5)),
+                material: materials.add(Color::GRAY),
+                transform: Transform::from_translation(segment_translation(chain_iterations)),
+                ..default()
+            },
+            chain_layers,
+            Name::from("Stake"),
+            Collider::cuboid(0.5, 2.0, 0.5),
+            RigidBody::Static,
+        ))
+        .id();
+
+    commands.spawn(
+        DistanceJoint::new(chain_last_entity, next_entity)
+            .with_limits(1.0, 1.5)
+            .with_local_anchor_1(Vec3::Y)
+            .with_local_anchor_2(Vec3::Y),
+    );
 }
 
 fn move_player(
     time: Res<Time>,
-    input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Velocity, With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut LinearVelocity, With<Player>>,
 ) {
     if let Ok(mut velocity) = query.get_single_mut() {
         let mut lat_speed = Vec3::ZERO;
 
-        if input.pressed(KeyCode::A) {
+        if input.pressed(KeyCode::KeyA) {
             lat_speed.x += 30.0 * time.delta_seconds();
         }
-        if input.pressed(KeyCode::D) {
+        if input.pressed(KeyCode::KeyD) {
             lat_speed.x -= 30.0 * time.delta_seconds();
         }
-        if input.pressed(KeyCode::W) {
+        if input.pressed(KeyCode::KeyW) {
             lat_speed.z += 30.0 * time.delta_seconds();
         }
-        if input.pressed(KeyCode::S) {
+        if input.pressed(KeyCode::KeyS) {
             lat_speed.z -= 30.0 * time.delta_seconds();
         }
 
         if lat_speed != Vec3::ZERO {
-            velocity.linvel += lat_speed.normalize_or_zero();
-        }
-    }
-}
-
-fn handle_ground_sensor(
-    rapier_context: Res<RapierContext>,
-    mut query: Query<(Entity, &mut GroundSensor, &Transform)>,
-) {
-    for (entity, mut ground_sensor, transform) in &mut query {
-        if let Some((_, hit_distance)) = rapier_context.cast_ray(
-            transform.translation,
-            Vec3::NEG_Y,
-            1.3,
-            true,
-            QueryFilter::default().exclude_collider(entity),
-        ) {
-            ground_sensor.grounded = true;
-            ground_sensor.ground_height = transform.translation.y - hit_distance;
-        } else {
-            ground_sensor.grounded = false;
+            velocity.0 = lat_speed.normalize_or_zero();
         }
     }
 }
 
 fn deploy_stake(
     mut commands: Commands,
-    input: Res<Input<KeyCode>>,
+    input: Res<ButtonInput<KeyCode>>,
     mut wire_info: ResMut<WireInfo>,
     wire_assets: Res<WireAssets>,
-    query: Query<(Entity, &Transform, &GroundSensor), With<Player>>,
+    query: Query<(Entity, &Transform), With<Player>>,
 ) {
-    if let Ok((entity, transform, sensor)) = query.get_single() {
-        if !wire_info.deployed && input.just_pressed(KeyCode::E) && sensor.grounded {
-            let stake_transform = Transform::from_xyz(
-                transform.translation.x,
-                sensor.ground_height + 0.5,
-                transform.translation.z,
-            );
+    if let Ok((entity, transform)) = query.get_single() {
+        if !wire_info.deployed && input.just_pressed(KeyCode::KeyE) {
+            let stake_transform = Transform::from_translation(transform.translation);
             wire_info.deployed = true;
             let stake_entity = commands
                 .spawn((
@@ -306,7 +271,7 @@ fn deploy_stake(
                         scene: wire_assets.stake.clone(),
                         ..default()
                     },
-                    RigidBody::Fixed,
+                    RigidBody::Static,
                     WirePrimary,
                     Collider::cuboid(0.25, 0.5, 0.25),
                     Stake,
@@ -356,54 +321,5 @@ fn handle_wire_length(
                 }
             }
         }
-    }
-}
-
-fn create_segments(
-    mut commands: Commands,
-    mut wire_info: ResMut<WireInfo>,
-    mut segment_events: EventReader<NewSegmentEvent>,
-) {
-    for event in segment_events.read() {
-        let NewSegmentEvent {
-            primary_position,
-            target_position,
-            primary_entity,
-            target_entity,
-        } = event;
-
-        let joint_to_primary = RopeJointBuilder::new()
-            .local_anchor1(Vec3::ZERO)
-            .local_anchor2(Vec3::ZERO)
-            .limits([0.0, 0.51]);
-
-        let joint_to_target = RopeJointBuilder::new()
-            .local_anchor1(Vec3::ZERO)
-            .local_anchor2(Vec3::ZERO)
-            .limits([0.0, 0.51])
-            .build();
-
-        let new_position = primary_position.lerp(*target_position, 0.5);
-
-        let new_entity = commands
-            .spawn((
-                SegmentBundle::new(
-                    new_position,
-                    ImpulseJoint::new(*target_entity, joint_to_target),
-                ),
-                WirePrimary,
-            ))
-            .id();
-        wire_info.primary_entity = Some(new_entity);
-
-        commands
-            .entity(*primary_entity)
-            .remove::<Sensor>()
-            .remove::<ImpulseJoint>()
-            .remove::<GravityScale>()
-            .remove::<WirePrimary>()
-            .insert(ImpulseJoint::new(new_entity, joint_to_primary));
-
-        wire_info.deployed_segments += 1;
     }
 }
