5f1380cc140a564b898d76ae58551ce3e2e6ef91
spooky-th-ghost <colin@wildheartinteractive.com>
2024-05-26 16:17:00 -0400

    integrated victimless-physics

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_xpbd_3d::prelude::*;
+use player::PlayerPlugin;
+use victimless_physics::prelude::*;
+
+mod chain;
+mod player;
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins,
-            bevy_xpbd_3d::prelude::PhysicsPlugins::default(),
-            // bevy_xpbd_3d::prelude::PhysicsDebugPlugin::default(),
+            VictimlessPhysicsPlugin,
             WorldInspectorPlugin::default(),
         ))
+        .add_plugins(PlayerPlugin)
         .register_type::<GroundSensor>()
         .register_type::<WireInfo>()
         .insert_resource(WireInfo::default())
@@ -17,11 +22,7 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(
             Update,
-            (
-                (deploy_stake, move_player, handle_wire_length),
-                apply_deferred,
-            )
-                .chain(),
+            ((deploy_stake, handle_wire_length), apply_deferred).chain(),
         )
         .run();
 }
@@ -80,13 +81,6 @@ impl Default for WireInfo {
     }
 }
 
-#[derive(PhysicsLayer, Clone, Copy, Debug)]
-pub enum Layers {
-    Ground,
-    Player,
-    Chain,
-}
-
 #[derive(Resource)]
 pub struct WireAssets {
     pub stake: Handle<Scene>,
@@ -112,16 +106,16 @@ pub struct GroundSensor {
     ground_height: f32,
 }
 
-#[derive(Component)]
-pub struct Player;
-
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
-    let chain_layers = CollisionLayers::new(Layers::Chain, Layers::Ground);
+    let chain_layers = CollisionLayers::new(
+        ColLayer::PlayerWidget,
+        [ColLayer::Object, ColLayer::Terrain],
+    );
 
     commands.insert_resource(WireAssets {
         stake: asset_server.load("stake.glb#Scene0"),
@@ -129,7 +123,7 @@ fn setup(
     });
 
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 10.0, -15.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 30.0, -45.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
@@ -141,116 +135,99 @@ fn setup(
             ..default()
         },
         Collider::cuboid(100.0, 2.0, 100.0),
-        CollisionLayers::new(Layers::Ground, Layers::Player),
+        CollisionLayers::new(
+            ColLayer::Terrain,
+            [
+                ColLayer::Character,
+                ColLayer::PlayerWidget,
+                ColLayer::Object,
+            ],
+        ),
         RigidBody::Static,
     ));
 
-    let mut next_entity = commands
-        .spawn((
-            PbrBundle {
-                mesh: meshes.add(Capsule3d {
-                    radius: 0.5,
-                    half_length: 0.5,
-                }),
-                material: materials.add(Color::RED),
-                ..default()
-            },
-            Player,
-            Name::from("Player"),
-            CollisionLayers::new(Layers::Player, Layers::Ground),
-            LockedAxes::ROTATION_LOCKED,
-            RigidBody::Dynamic,
-            Collider::capsule(0.5, 0.5),
-            GroundSensor::default(),
-        ))
-        .id();
+    commands.spawn((
+        PbrBundle {
+            transform: Transform::from_xyz(10.0, 5.0, 0.0),
+            mesh: meshes.add(Cuboid::new(5.0, 5.0, 5.0)),
+            material: materials.add(Color::PURPLE),
+            ..default()
+        },
+        Collider::cuboid(5.0, 5.0, 5.0),
+        CollisionLayers::new(
+            ColLayer::Object,
+            [
+                ColLayer::Character,
+                ColLayer::PlayerWidget,
+                ColLayer::Terrain,
+            ],
+        ),
+        RigidBody::Dynamic,
+    ));
 
     fn segment_translation(i: usize) -> Vec3 {
         let starting_translation = Vec3::new(-4.0, -1.0, 4.0);
         (0.3 * i as f32 * Vec3::X) + starting_translation
     }
 
-    let chain_iterations = 20;
+    // let chain_iterations = 20;
 
-    for i in 0..chain_iterations {
-        let previous_entity = next_entity;
-        let translation = segment_translation(i);
-        next_entity = commands
-            .spawn((
-                PbrBundle {
-                    mesh: meshes.add(Capsule3d {
-                        radius: 0.125,
-                        half_length: 0.125,
-                    }),
-                    material: materials.add(Color::BLUE),
-                    transform: Transform::from_translation(translation),
-                    ..default()
-                },
-                RigidBody::Dynamic,
-                Collider::capsule(0.5, 0.25),
-                chain_layers,
-                Name::from("Link"),
-            ))
-            .id();
+    // for i in 0..chain_iterations {
+    //     let previous_entity = next_entity;
+    //     let translation = segment_translation(i);
+    //     next_entity = commands
+    //         .spawn((
+    //             PbrBundle {
+    //                 mesh: meshes.add(Capsule3d {
+    //                     radius: 0.125,
+    //                     half_length: 0.25,
+    //                 }),
+    //                 material: materials.add(Color::BLUE),
+    //                 transform: Transform::from_translation(translation)
+    //                     .with_rotation(Quat::from_axis_angle(Vec3::Z, 90.0_f32.to_radians())),
+    //                 ..default()
+    //             },
+    //             RigidBody::Dynamic,
+    //             Collider::capsule(0.5, 0.125),
+    //             Friction::new(1.0),
+    //             chain_layers,
+    //             Name::from("Link"),
+    //         ))
+    //         .id();
 
-        commands.spawn(
-            DistanceJoint::new(previous_entity, next_entity)
-                .with_limits(0.01, 0.05)
-                .with_local_anchor_1(Vec3::Y)
-                .with_local_anchor_2(Vec3::NEG_Y),
-        );
-    }
+    //     commands.spawn(
+    //         DistanceJoint::new(previous_entity, next_entity)
+    //             .with_limits(0.3, 0.5)
+    //             .with_local_anchor_1(Vec3::Y * 0.5)
+    //             .with_local_anchor_2(Vec3::NEG_Y * 0.5)
+    //             .with_compliance(0.0), // .with_linear_velocity_damping(500.0), // .with_rest_length(0.01),
+    //     );
+    // }
 
-    let chain_last_entity = next_entity;
+    // let chain_last_entity = next_entity;
 
-    next_entity = commands
-        .spawn((
-            PbrBundle {
-                mesh: meshes.add(Cuboid::new(0.5, 2.0, 0.5)),
-                material: materials.add(Color::GRAY),
-                transform: Transform::from_translation(segment_translation(chain_iterations)),
-                ..default()
-            },
-            chain_layers,
-            Name::from("Stake"),
-            Collider::cuboid(0.5, 2.0, 0.5),
-            RigidBody::Static,
-        ))
-        .id();
+    // next_entity = commands
+    //     .spawn((
+    //         PbrBundle {
+    //             mesh: meshes.add(Cuboid::new(0.5, 2.0, 0.5)),
+    //             material: materials.add(Color::GRAY),
+    //             transform: Transform::from_translation(segment_translation(chain_iterations)),
+    //             ..default()
+    //         },
+    //         chain_layers,
+    //         Name::from("Stake"),
+    //         Collider::cuboid(0.5, 2.0, 0.5),
+    //         RigidBody::Static,
+    //     ))
+    //     .id();
 
-    commands.spawn(
-        DistanceJoint::new(chain_last_entity, next_entity)
-            .with_limits(1.0, 1.5)
-            .with_local_anchor_1(Vec3::Y)
-            .with_local_anchor_2(Vec3::Y),
-    );
-}
-
-fn move_player(
-    time: Res<Time>,
-    input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut LinearVelocity, With<Player>>,
-) {
-    if let Ok(mut velocity) = query.get_single_mut() {
-        let mut lat_speed = Vec3::ZERO;
-
-        if input.pressed(KeyCode::KeyA) {
-            lat_speed.x += 30.0 * time.delta_seconds();
-        }
-        if input.pressed(KeyCode::KeyD) {
-            lat_speed.x -= 30.0 * time.delta_seconds();
-        }
-        if input.pressed(KeyCode::KeyW) {
-            lat_speed.z += 30.0 * time.delta_seconds();
-        }
-        if input.pressed(KeyCode::KeyS) {
-            lat_speed.z -= 30.0 * time.delta_seconds();
-        }
-
-        if lat_speed != Vec3::ZERO {
-            velocity.0 = lat_speed.normalize_or_zero();
-        }
-    }
+    // commands.spawn(
+    //     DistanceJoint::new(chain_last_entity, next_entity)
+    //         .with_local_anchor_1(Vec3::Y)
+    //         .with_local_anchor_2(Vec3::Y)
+    //         .with_limits(1.0, 2.0)
+    //         .with_compliance(0.0), // .with_linear_velocity_damping(500.0), // .with_rest_length(0.01),
+    // );
 }
 
 fn deploy_stake(
@@ -258,7 +235,7 @@ fn deploy_stake(
     input: Res<ButtonInput<KeyCode>>,
     mut wire_info: ResMut<WireInfo>,
     wire_assets: Res<WireAssets>,
-    query: Query<(Entity, &Transform), With<Player>>,
+    query: Query<(Entity, &Transform), With<player::Player>>,
 ) {
     if let Ok((entity, transform)) = query.get_single() {
         if !wire_info.deployed && input.just_pressed(KeyCode::KeyE) {
---
+++ src/player.rs
@@ -0,0 +1,71 @@
+use bevy::prelude::*;
+use bevy_xpbd_3d::prelude::*;
+use victimless_physics::prelude::*;
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_player)
+            .add_systems(Update, set_player_direction);
+    }
+}
+
+// types
+#[derive(Component)]
+pub struct Player;
+
+// systems
+fn spawn_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Capsule3d {
+                radius: 0.5,
+                half_length: 0.5,
+            }),
+            material: materials.add(Color::RED),
+            ..default()
+        },
+        CharacterBundle::player().with_move_speed(200.0),
+        Player,
+        Name::from("Player"),
+    ));
+}
+
+fn set_player_direction(
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut MoveDirection, &mut MoveSpeed, Has<Grounded>), With<Player>>,
+) {
+    for (mut direction, mut speed, is_grounded) in &mut query {
+        if is_grounded {
+            let mut lat_dir = Vec3::ZERO;
+
+            if input.pressed(KeyCode::KeyA) {
+                lat_dir.x += 1.0;
+            }
+            if input.pressed(KeyCode::KeyD) {
+                lat_dir.x -= 1.0;
+            }
+            if input.pressed(KeyCode::KeyW) {
+                lat_dir.z += 1.0;
+            }
+            if input.pressed(KeyCode::KeyS) {
+                lat_dir.z -= 1.0;
+            }
+
+            direction.set(lat_dir);
+
+            if direction.started_moving() {
+                speed.start_moving();
+            }
+
+            if direction.stopped_moving() {
+                speed.stop_moving();
+            }
+        }
+    }
+}
